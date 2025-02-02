fe4ae7dc9910a20de2cd5ce9ae5264adecb0325c
AbdullahSharif <abdullah.29.umar@gmail.com>
2025-02-02 14:56:32 +0500

    added the Schemas for corresponding emails

---
+++ app/schemas.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class UserCreate(SQLModel):
+    name: str
+    username: str
+    email: str
+    password: str
+
+class UserRead(SQLModel):
+    id: int
+    name: str
+    username: str
+    email: str
+
+class UserCreateInternal(SQLModel):
+    name: str
+    username: str
+    email: str
+    hashed_password: str
+
+# -------email request--------
+class EmailRequest(SQLModel):
+    user_id: int
+    user_input: str
+    reply_to : Optional[str] = Field(default=None)
+    context: Optional[str] = Field(default=True)
+    lenght: Optional[int] = Field(default=True)
+    tone: str
+    generated_email: str
+    timestamp: datetime = Field(
+        default_factory=lambda: datetime.now("utc")
+    )
+
+class EmailLogRead(SQLModel):
+    user_id: int
+    user_input: str
+    reply_to : Optional[str] = Field(default=None)
+    context: Optional[str] = Field(default=True)
+    lenght: Optional[int] = Field(default=True)
+    tone: str
+    generated_email: str
+    timestamp: datetime
+
+
+
